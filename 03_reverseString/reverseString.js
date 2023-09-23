8ba9f6bc9bfa64cb4818f6bb3a364563d4ab8fb6
Ndzi Nfor <ndzinfor@gmail.com>
2023-09-23 15:24:16 +0100

    complete exercise 3_stringReverse solution

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,7 @@
-const reverseString = function() {
-
-};
-
+let reverseString = function(input) {
+    
+    return input.split("").reverse().join("");
+    
+}
 // Do not edit below this line
 module.exports = reverseString;
