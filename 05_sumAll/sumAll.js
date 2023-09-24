7add7c777df2fd3774ff33765d7b831904a62909
Ndzi Nfor <ndzinfor@gmail.com>
2023-09-24 23:59:15 +0100

    complete ex 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+  let sum = 0;
+  for (let i = min; i <= max; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
