e4e06a75b3922348acb0f6107158cc4eaddbbd3e
Ndzi Nfor <ndzinfor@gmail.com>
2023-09-24 23:04:39 +0100

    complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    let newArray = [];
+    array.foreach((item)=>{
+        if(!args.includes(item)){
+            newArray.push(item);
+        }
+    });
+    return newArray;
 };
 
 // Do not edit below this line
