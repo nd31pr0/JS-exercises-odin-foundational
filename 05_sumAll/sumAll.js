63fc9ff7529d5f959e2c63b27f1517dfc1a85ddb
Ndzi Nfor <ndzinfor@gmail.com>
2023-09-25 00:04:11 +0100

    refactor solution for ex 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,10 +1,9 @@
-const sumAll = function(a,b) {
+const sumAll = function(min,max) {
   let sum = 0;
   for (let i = min; i <= max; i++) {
-    sum += i;
+    sum = sum + i;
   }
   return sum;
 };
 
-// Do not edit below this line
 module.exports = sumAll;
