1f2a828a516d0b766afad3219e70faffa1b44577
Ndzi Nfor <ndzinfor@gmail.com>
2023-10-10 11:28:24 +0100

    complete fibonacci function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+let F = [0, 1];
+for(let i = 2; i <= num; i++) {
+    F.push( F[i - 1] + F[i - 2])
+    }
+if(num < 1) return "OOPS";
+return(F[num])
 
 };
 
 // Do not edit below this line
+
 module.exports = fibonacci;
