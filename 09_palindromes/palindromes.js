bed86f86d191d515cced900a97378297070969df
Ndzi Nfor <ndzinfor@gmail.com>
2023-10-10 10:22:26 +0100

    complete palindromes to pass test

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,25 @@
-const palindromes = function () {
+// Algorithm
+// - remove space and punctuation from String
+// - reverse the string and store in new variable
+// - compare reversed string and string with punctuation and space removed. 
+// - if both are same, return true else false.
 
+const palindromes = function (text) {
+    const cleanText = text.replace(/[^\w\s\']|_/g, "")
+                          .replace(/\s+/g, "").toLowerCase();
+                          
+    // To reverse string, we need to separate the xters, reverse and then join again                      
+    const revText = cleanText.split("").reverse().join("");
+    return (cleanText === revText) ? true : false;
+    //console.log(result);
+    // return result;               
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+
+
+
+console.log(palindromes("Hey eh"));
+
+
