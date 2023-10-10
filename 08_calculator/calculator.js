45c83290da1f483c34f632dfc7c76cc4d1eecfe8
Ndzi Nfor <ndzinfor@gmail.com>
2023-10-10 08:50:42 +0100

    Complete calculator exercises and tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
+const add = function(a,b) {
+	return a+b;
+};
+
+const subtract = function(a,b) {
+	return a-b;
+};
+
+const sum = function(array) {
+	array.reduce((total, current) => {return total + current}, 0)
+        
+    };
+
+const multiply = function(array) {
+    return array.reduce((product, current) => {
+        product * current
+    })
+
+};
+
+const power = function(a,b) {
+    return Math.pow(a,b);
 	
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(num) {
+    if (num === 0) return 1;
+    let result = 1;
+	while (num > 1){
+        result = result * num
+        num --
+    }
+    console.log(result);
 };
 
 // Do not edit below this line
